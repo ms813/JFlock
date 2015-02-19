999e74a553c432d4c29db8358bd90478bdc4031d
ms813 <matthew.smith813@gmail.com>
2015-02-19 17:27:24 +0000

    Refactored BirdFactory into registration model

--- src/BirdFactory.java
+++ src/BirdFactory.java
@@ -3,6 +3,7 @@ import org.jsfml.graphics.Sprite;
 import org.jsfml.system.Vector2f;
 import org.jsfml.system.Vector2i;
 
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -12,34 +13,32 @@ public class BirdFactory {
 
     private Vector2i windowSize;
 
+    private static HashMap<BirdSpecies, BirdCreationInstructions> registeredBirdSpecies = new HashMap<BirdSpecies, BirdCreationInstructions>();
+
     public BirdFactory(Vector2i windowSize){
         this.windowSize = windowSize;
+    }
+
+    public static void registerBirdSpecies(BirdSpecies species, BirdCreationInstructions bc){
+        if(!registeredBirdSpecies.containsKey(species)){
+            registeredBirdSpecies.put(species, bc);
+        } else{
+            System.out.println(species + " already registered!");
+        }
     }
 
     public Bird createBird(BirdSpecies species, Vector2f position){
         Random rnd = new Random();
 
         Bird b = new Bird();
-        Sprite s;
-        if(species == BirdSpecies.SPARROW){
 
-            String txtName = "bird" +  rnd.nextInt(5);
-            s = new Sprite(TextureLibrary.getTexture(txtName));
-            b.setSpecies(BirdSpecies.SPARROW);
-            b.setMotionHandler(new SparrowMotionHandler());
-
-        } else if(species == BirdSpecies.HAWK){
-
-            s = new Sprite(TextureLibrary.getTexture("hawk"));
-            b.setSpecies(BirdSpecies.HAWK);
-            b.setMotionHandler(new HawkMotionHandler());
-
-        } else {
-            s = new Sprite(TextureLibrary.getTexture("bird0"));
+        if(registeredBirdSpecies.containsKey(species)){
+            BirdCreationInstructions bc = registeredBirdSpecies.get(species);
+            bc.setSpecies(b);
+            bc.setMotionHandler(b);
+            bc.setSprite(b, position, 0.05f);
         }
-        s.setScale(0.05f, 0.05f);
-        s.setPosition(position);
-        b.setSprite(s);
+
         b.setVelocity(VMath.rndVector2f());
         b.init();
 
@@ -49,7 +48,7 @@ public class BirdFactory {
     //if no position passed return bird with random position
     public Bird createBird(BirdSpecies species){
         Random rnd = new Random();
-        Vector2f rndPos = new Vector2f(rnd.nextFloat() * windowSize.x, rnd.nextFloat()*windowSize.y);
+        Vector2f rndPos = new Vector2f(rnd.nextFloat() * windowSize.x, rnd.nextFloat() * windowSize.y);
         return createBird(species, rndPos);
     }
 }
